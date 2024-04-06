026a306e455db0d4547bad3191d711cef722d952
rajababuagp <134359930+rajababuagp@users.noreply.github.com>
2024-04-06 10:51:05 +0530

    add expense is done

--- expense/admin.py
+++ expense/admin.py
@@ -2,32 +2,19 @@ from django.contrib import admin
 # from .models import User, Expense, ExpenseTypes, SplitExpense
 from .models import User, Expense, ExpenseParticipant
 
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = ('user_id', 'name', 'email', 'mobile_number')
-#     search_fields = ('name', 'email', 'mobile_number')
 
-# class ExpenseTypesAdmin(admin.ModelAdmin):
-#     list_display = ('expense_type_id', 'expense_type')
-#     search_fields = ('expense_type',)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'userId', 'name', 'email', 'mobile_number')
+    search_fields = ('name', 'email', 'mobile_number')
 
-# class ExpenseAdmin(admin.ModelAdmin):
-#     list_display = ('expense_id', 'paid_by', 'amount', 'description', 'date')
-#     list_filter = ('paid_by',)
-#     search_fields = ('paid_by__name', 'description')
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'payer', 'amount', 'expense_type', 'created_at')
+    list_filter = ('payer', 'expense_type')
+    search_fields = ('payer__name',)
 
-# # class SplitExpenseAdmin(admin.ModelAdmin):
-# #     list_display = ('split_expense_id', 'expense', 'user', 'expense_type', 'amount')
-# #     list_filter = ('expense__paid_by', 'user', 'expense_type')
-# #     search_fields = ('expense__paid_by__name', 'user__name')
+class ExpenseParticipantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'expense', 'user', 'share')
 
-# admin.site.register(User, UserAdmin)
-# admin.site.register(ExpenseTypes, ExpenseTypesAdmin)
-# admin.site.register(Expense, ExpenseAdmin)
-# admin.site.register(SplitExpense)
-
-
-
-
-admin.site.register(User)
-admin.site.register(Expense)
-admin.site.register(ExpenseParticipant)
+admin.site.register(User, UserAdmin)
+admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(ExpenseParticipant, ExpenseParticipantAdmin)
